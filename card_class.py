f7277a8c9032a2fe48c36be08b7df812a66e83a6
Jonathan <jonathanbeaty@pm.me>
2022-10-14 20:16:26 -0500

    updating files

--- card_class.py
+++ card_class.py
@@ -125,6 +125,24 @@ def take_bet(chips):
             else:
                 break
 
+def hit_or_stand(deck,hand):
+    global playing  # to control an upcoming while loop
+    
+    while True:
+        x = input("Would you like to Hit or Stand? Enter 'h' or 's' ")
+        
+        if x[0].lower() == 'h':
+            hit(deck,hand)  # hit() function defined above
+
+        elif x[0].lower() == 's':
+            print("Player stands. Dealer is playing.")
+            playing = False
+
+        else:
+            print("Sorry, please try again.")
+            continue
+        break
+
 
 
 
