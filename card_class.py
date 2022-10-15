69c541c874cc03f15ebb9eef5887ffa1308f99e5
Jonathan <jonathanbeaty@pm.me>
2022-10-14 20:09:13 -0500

    updating files

--- card_class.py
+++ card_class.py
@@ -98,9 +98,32 @@ class Hand():
 
 class Chips():
 
-    def __init__():
-        pass
+    def __init__(self, total=100):
+        self.total = total
+        self.bet = 0
 
+    def win_bet(self):
+        self.total += self.bet
+    
+    def lose_bet(self):
+        self.total -= self.bet
+
+
+# *********************************************************************************************************************************************************
+# FUNCTIONS
+
+def take_bet(chips):
+    
+    while True:
+        try:
+            chips.bet = int(input('How many chips would you like to bet? '))
+        except ValueError:
+            print('Sorry, a bet must be an integer!')
+        else:
+            if chips.bet > chips.total:
+                print("Sorry, your bet can't exceed",chips.total)
+            else:
+                break
 
 
 
