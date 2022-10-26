c90e99605d5f3192e74424c190dec0a977bf242a
Jonathan <jonathanbeaty@pm.me>
2022-10-25 22:00:19 -0500

    updating files

--- card_class.py
+++ card_class.py
@@ -143,6 +143,21 @@ def hit_or_stand(deck,hand):
             continue
         break
 
+def show_some(player,dealer):
+    print("\nDealer's Hand:")
+    print(" <card hidden>")
+    print('',dealer.cards[1])  
+    print("\nPlayer's Hand:", *player.cards, sep='\n ')
+    
+def show_all(player,dealer):
+    print("\nDealer's Hand:", *dealer.cards, sep='\n ')
+    print("Dealer's Hand =",dealer.value)
+    print("\nPlayer's Hand:", *player.cards, sep='\n ')
+    print("Player's Hand =",player.value)
+
+    
+
+
 
 
 
