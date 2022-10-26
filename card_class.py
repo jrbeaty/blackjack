cc9b89f3fcec727b800040bb11dbaf710d5138a1
Jonathan <jonathanbeaty@pm.me>
2022-10-25 22:05:17 -0500

    final commit

--- card_class.py
+++ card_class.py
@@ -155,22 +155,87 @@ def show_all(player,dealer):
     print("\nPlayer's Hand:", *player.cards, sep='\n ')
     print("Player's Hand =",player.value)
 
+def player_busts(player,dealer,chips):
+    print("Player busts!")
+    chips.lose_bet()
+
+def player_wins(player,dealer,chips):
+    print("Player wins!")
+    chips.win_bet()
+
+def dealer_busts(player,dealer,chips):
+    print("Dealer busts!")
+    chips.win_bet()
     
+def dealer_wins(player,dealer,chips):
+    print("Dealer wins!")
+    chips.lose_bet()
+    
+def push(player,dealer):
+    print("Dealer and Player tie! It's a push.")
 
+while True:
+    # Print an opening statement
+    print('Welcome to BlackJack! Get as close to 21 as you can without going over!\n\
+    Dealer hits until she reaches 17. Aces count as 1 or 11.')
+    
+    # Create & shuffle the deck, deal two cards to each player
+    deck = Deck()
+    deck.shuffle()
+    
+    player_hand = Hand()
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+    
+    dealer_hand = Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+            
+    player_chips = Chips()   
+    
+    take_bet(player_chips)
+    
+    show_some(player_hand,dealer_hand)
+    
+    while playing:
+        
+        hit_or_stand(deck,player_hand) 
+        
+        show_some(player_hand,dealer_hand)  
+        
+        if player_hand.value > 21:
+            player_busts(player_hand,dealer_hand,player_chips)
+            break         
 
+    if player_hand.value <= 21:
+        
+        while dealer_hand.value < 17:
+            hit(deck,dealer_hand)    
+    
+        show_all(player_hand,dealer_hand)
+        
+        if dealer_hand.value > 21:
+            dealer_busts(player_hand,dealer_hand,player_chips)
 
+        elif dealer_hand.value > player_hand.value:
+            dealer_wins(player_hand,dealer_hand,player_chips)
 
+        elif dealer_hand.value < player_hand.value:
+            player_wins(player_hand,dealer_hand,player_chips)
 
-'''
-test_deck = Deck()
-test_deck.shuffle()
-
-test_player = Hand()
-pulled_card = test_deck.deal()
-print(pulled_card)
-test_player.add_card(pulled_card)
-print(test_player.value)
-'''
+        else:
+            push(player_hand,dealer_hand)        
+    
+    print("\nPlayer's winnings stand at",player_chips.total)
+    
+    new_game = input("Would you like to play another hand? Enter 'y' or 'n' ")
+    
+    if new_game[0].lower()=='y':
+        playing=True
+        continue
+    else:
+        print("Thanks for playing!")
+        break
 
 
 
